cab55d895caf3004f109b1df9711a737e9fe8007
Zeus <97366514+AchievedZeus574@users.noreply.github.com>
2024-12-09 00:51:51 -0600

    added random post function

--- pycord.py
+++ pycord.py
@@ -3,7 +3,9 @@ import os
 from dotenv import load_dotenv
 import logging
 import asyncpraw
-import randoms
+import randoms #used for other functions 
+from random import choice
+import asyncio
 
 #load .env variables
 load_dotenv()
@@ -13,6 +15,30 @@ logger= logging.getLogger('discord')
 logger.setLevel(logging.INFO)
 handler= logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+
+#grab information for async praw
+reddit= asyncpraw.Reddit(
+    client_id= os.getenv('REDDIT_CLIENT_ID'),
+    client_secret= os.getenv('REDDIT_CLIENT_SECRET'),
+    user_agent= os.getenv('REDDIT_USER_AGENT')
+)
+
+#grab random image from reddit specified by command
+async def post_grab(sub):
+    try:
+        subreddit= await reddit.subreddit(sub)
+        posts= [
+            post async for post in subreddit.hot(limit=25)
+            if post.url.endswith(('.jpg', '.png', '.jpeg'))
+        ]
+        if posts:
+            random_post= choice(posts)
+            return random_post.url
+        else:
+            return f"No image posts found in r/{sub}."
+    except Exception as e:
+        return f"An error occured {str(e)}"
+
 
 #create bot and get it ready on startup
 bot= discord.Bot()
@@ -26,6 +52,11 @@ async def on_ready():
 @bot.slash_command(name="hello", description="say hi to Jeff")
 async def hello(ctx: discord.ApplicationContext):
     await ctx.respond("Hey, "+ randoms.aliase()+ "!")
+
+@bot.slash_command(name="random", description="Grab random image from best of a SubReddit of your choosing")
+async def random(ctx, subreddit: discord.Option(discord.SlashCommandOptionType.string)):
+    url= await post_grab(subreddit)
+    await ctx.respond(url)
 
 #run the bot
 bot.run(os.getenv('TOKEN'))
