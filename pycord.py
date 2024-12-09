8c1ff565052e7f71ebb45a06dbff881a6d78282e
Zeus <97366514+AchievedZeus574@users.noreply.github.com>
2024-12-08 20:32:16 -0600

    starting to implement pycord
    I understand it better, and will be moving to it in the future

---
+++ pycord.py
@@ -0,0 +1,31 @@
+import discord
+import os
+from dotenv import load_dotenv
+import logging
+import asyncpraw
+import randoms
+
+#load .env variables
+load_dotenv()
+
+#enable logging at INFO level
+logger= logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler= logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+
+#create bot and get it ready on startup
+bot= discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} ready')
+    await bot.sync_commands()
+
+#commands for bot
+@bot.slash_command(name="hello", description="say hi to Jeff")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hey, "+ randoms.aliase()+ "!")
+
+#run the bot
+bot.run('MTAwMjM0OTExNzQ3OTA1OTQ1Ng.G5cWj7.ARDo0NVWSE4ZkKUh40D712wzaHnf0Z5P3nCpA0')
