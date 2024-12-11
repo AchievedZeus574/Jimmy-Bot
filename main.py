5184c97dd269a9775a228612051edce23475eb04
Zeus <97366514+AchievedZeus574@users.noreply.github.com>
2024-12-11 02:05:59 -0600

    made CotD automatic (I think)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import logging
@@ -7,7 +8,7 @@ import randoms #used for other functions
 from random import choice
 import asyncio
 import configparser
-from datetime import date
+from datetime import date, datetime, timedelta
 
 #load .env variables
 load_dotenv()
@@ -19,8 +20,9 @@ config.read('config.ini')
 #get settings from config.ini
 SubList= config.get("bot_settings", "SUBREDDIT_LIST", fallback="cats, cat").split(",")
 CotD_CiD= int(os.getenv('TARGET_CHANNEL_ID'))
-PostTime= config.get("bot_settings", "POST_HOUR", fallback= 12)
+PostTime= int(config.get("bot_settings", "POST_HOUR", fallback= 12))
 LogLevel = getattr(logging, config.get('bot_settings', 'LOG_LEVEL', fallback='WARNING').upper(), logging.WARNING)
+AllowedRole= config.get("bot_settings", "COTD_ROLE")
 
 #enable logging for pycord
 logger = logging.getLogger('discord')
@@ -31,9 +33,9 @@ logger.addHandler(handler)
 
 #enable logging for CotD autopost
 CotD_Log= "CotD_Log.log"
-def CotD_Logging(CotDinfo, Sub):
+def CotD_Logging(CotDinfo):
     with open("CotD_Log.log", "a") as log:
-        log.write(f'Date: {date.today()}, Subreddit: {Sub}, Post ID: \n')
+        log.write(f'Date: {date.today()}, Subreddit: {CotDinfo.subreddit.display_name}, Post ID: {CotDinfo.id}\n')
 
 #grab information for async praw
 reddit= asyncpraw.Reddit(
@@ -58,24 +60,31 @@ async def post_grab(sub):
     except Exception as e:
         return f"An error occured {str(e)}"
 
-
 #post CotD in channel of choosing then increment counter
-async def auto_post(CiD= CotD_CiD):
+async def Post_CotD(CiD= CotD_CiD):
     try:
         CotD_channel= bot.get_channel(CiD)
         if CotD_channel is None:
             print("Channel not found.")
             return
         #get post from reddit and send it
-        RanSub= choice(SubList)
-        post= await post_grab(RanSub)
+        post= await post_grab(choice(SubList))
         await CotD_channel.send(post.url)
-        CotD_Logging(post, RanSub)
+        CotD_Logging(post)
     except ValueError:
         print(f'Invalid Channel ID')
     except Exception as e:
         print(f'Error: {e}')
 
+#schedule the CotD Post to run once a day at PostTime
+async def autopost():
+    while True:
+        now= datetime.now()
+        TargetTime= now.replace(hour= PostTime)
+        if now >= TargetTime:
+            TargetTime+= timedelta(days= 1)
+        delay= (TargetTime- now).total_seconds()
+        await asyncio.sleep(delay)
 
 #create bot and get it ready on startup
 bot= discord.Bot()
@@ -84,6 +93,7 @@ bot= discord.Bot()
 async def on_ready():
     print(f'{bot.user} ready')
     await bot.sync_commands()
+    bot.loop.create_task(autopost())
 
 #commands for bot
 @bot.slash_command(name="hello", description="say hi to Jeff")
@@ -103,9 +113,10 @@ async def random(ctx, subreddit: discord.Option(discord.SlashCommandOptionType.s
     await ctx.respond(url.url)
 
 @bot.slash_command(name="triggerpost", description="Trigger posting of CotD")
+#@commands.has_role(AllowedRole)
 async def CotDM(ctx):
     await ctx.defer(ephemeral= True)
-    post= await auto_post()
+    post= await Post_CotD()
     await ctx.followup.send(f"Posted in <#{CotD_CiD}>")
 
 #run the bot
